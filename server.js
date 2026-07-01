e0af79f1571c528e98253b8a5e78c6039402ed9d
Dinesh Mali <dineshdattatraymali@gmail.com>
2026-07-01 12:05:29 +0530

    feat: add CORS support, update API endpoint, and integrate content dashboards and status checkers into the UI

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
+app.use(cors());
 const PORT = 3000;
-const API_URL = 'http://192.168.1.9:8080';
+const API_URL = 'http://localhost:8081';
 
-// const API_URL = 'http://13.200.242.254:8080/';
+// const API_URL = 'http://192.168.1.9:8080';
 
 // Serve the index.html file and any static assets
 app.use(express.static(__dirname));
@@ -20,6 +22,7 @@ app.use('/api', (req, res, next) => {
 }, createProxyMiddleware({
   target: API_URL,
   changeOrigin: true,
+  logLevel: 'debug',
 }));
 
 // Send index.html for the root route
